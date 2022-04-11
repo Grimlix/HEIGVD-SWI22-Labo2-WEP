89ca03b94e07039b65107e96380bc6ce803214c9
lucas <lucas.gianinetti@heig-vd.ch>
2022-04-11 23:18:09 +0200

    partie 2 fonctionne, partie 3 presque

--- files/manual-encryption.py
+++ files/manual-encryption.py
@@ -11,7 +11,12 @@ from scapy.layers.dot11 import RadioTap
 
 #Cle wep AA:AA:AA:AA:AA
 key= b'\xaa\xaa\xaa\xaa\xaa'
-message = b'\xaa' * 9 # Taille de 9 minimum sinon le ICV ne se vérifie pas
+message = b'hello'
+
+#La taille du message minimum doit être de 9 bytes sinon l'ICV ne se vérifie pas
+#On ajoute donc neuf fois le caractère au début du message si il fait moins que 9 caractères
+if len(message) < 9:
+    message = (b'\x00' * 9) + message
 iv = b'bit'
 
 # ON récupère le fichier pcap d'avant pour le copier et juste changer ses valeurs
@@ -28,7 +33,7 @@ icv = binascii.crc32(message)
 
 # Encrypt
 ciphertext = cipher.crypt(message + struct.pack('I', icv))
-icv_encrypted = struct.unpack('!L', ciphertext[-4:])[0]
+icv_encrypted = struct.unpack('!L', ciphertext[-4:])[0] #icv représente les 4 dernier bytes
 
 # On change les valeurs du paquet 
 ciphertext = ciphertext[:-4]
@@ -43,7 +48,7 @@ print ("IV :  " + iv.hex())
 arp[RadioTap].len = None
 
 # On écrit dans un nouveau fichier.
-wrpcap('filtered.pcap', arp)
+wrpcap('trame_chiffree.pcap', arp)
 
 
 
